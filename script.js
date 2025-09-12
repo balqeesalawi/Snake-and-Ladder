b168ea01dab306d4a6af2e67def51887bd3e23f4
Balqeesalawi <balqeesalawi195@gmail.com>
2025-09-12 16:47:19 +0300

    changing the dice function to include moving also

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const squares = document.querySelectorAll(".square")
 const snake = "snake"
 const ladder = "ladder"
 const dice = document.querySelector("#dice")
+let randomNum = 0
+let steps = 0
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const getRandomNumber = () => {
-  const randomNum = Math.ceil(Math.random() * 6)
+  randomNum = Math.ceil(Math.random() * 6)
   dice.innerText = randomNum
+  if (randomNum) {
+    steps = squares[randomNum].append(player)
+  }
+  console.log(`steps=${steps}`)
 }
+
+const movingPlayer = () => {}
 
 dice.addEventListener("click", getRandomNumber)
