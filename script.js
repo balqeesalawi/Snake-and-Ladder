2278ff76ce0894f329acc951633375e13f8fe7ce
Balqeesalawi <balqeesalawi195@gmail.com>
2025-09-12 14:01:21 +0300

    added style and dice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("connected")
+let player = document.querySelector(".player")
+const squares = document.querySelectorAll(".square")
+const snake = "snake"
+const ladder = "ladder"
+const dice = document.querySelector("#dice")
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomNumber = () => {
+  const randomNum = Math.ceil(Math.random() * 6)
+  dice.innerText = randomNum
+}
+
+dice.addEventListener("click", getRandomNumber)
