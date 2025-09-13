efd4d99c1335e5a118310eb9bd5d46a1a0dd9316
Balqeesalawi <balqeesalawi195@gmail.com>
2025-09-13 19:08:46 +0300

    10 squares, 1 player, 1 ladder and 1 snake

--- script.js
+++ script.js
@@ -13,25 +13,40 @@ const getRandomNumber = () => {
   randomNum = Math.ceil(Math.random() * 6)
   dice.innerText = randomNum
   steps += randomNum
-  squares[steps - 1].append(player)
-  console.log(steps)
 
-  if (squares[steps - 1].innerText === "5 Ladder") {
-    squares[steps + 3].append(player)
-    steps = parseInt(squares[steps + 3].innerText)
+  if (steps < 10) {
+    squares[steps - 1].appendChild(player)
     console.log(steps)
-  } else if (squares[steps - 1].innerText === "8 Snake") {
-    squares[steps - 6].append(player)
-    steps = parseInt(squares[steps - 6].innerText)
+    if (squares[steps - 1].innerText === "5 Ladder") {
+      squares[steps + 3].appendChild(player) //taking player from 5 to 8
+      steps = parseInt(squares[steps + 3].innerText)
+      console.log(steps)
+    } else if (squares[steps - 1].innerText === "8 Snake") {
+      squares[steps - 6].appendChild(player) // taking player from 8 to 3
+      steps = parseInt(squares[steps - 6].innerText)
+      console.log(steps)
+    }
+  } else if (steps > 10) {
+    steps = steps - randomNum
     console.log(steps)
-  }
-
-  if (steps === 10) {
+  } else {
+    squares[steps - 1].appendChild(player)
     winner.innerText = "Winner!"
   }
+
+  // } else if (steps === 10) {
+  //   winner.innerText = "Winner!"
+  // } else {
+
+  //   let finalSteps = 0
+  //   finalSteps = randomNum
+  //   console.log(finalSteps)
+  //   console.log(steps)
+  // if (currentSquare + finalSteps === 10) {
+  //   squares[9].appendChild(player)
+  //   winner.innerText = "Winner!"
+  // }
+  // }
 }
 
-const movingPlayer = () => {}
-
 dice.addEventListener("click", getRandomNumber)
-dice.addEventListener("click", movingPlayer)
