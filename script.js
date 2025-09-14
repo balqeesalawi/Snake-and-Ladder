9154a0bf47fff1cf3063d07ffe37f93545e9625c
Balqeesalawi <balqeesalawi195@gmail.com>
2025-09-14 15:20:50 +0300

    make divs from javaScript + added more players

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 let player = document.querySelector(".player")
-const squares = document.querySelectorAll(".square")
 const snake = document.querySelector(".snake")
 const ladder = document.querySelector(".ladder")
 const dice = document.querySelector("#dice")
 const winner = document.querySelector("#header")
+const playersNum = document.querySelectorAll(".players")
+
+let game = document.querySelector("#game")
 
 let randomNum = 0
 let steps = 0
+
+const playerNumber = () => {
+  if (playersNum[0]) {
+    let newPlayer = document.createElement("div")
+    newPlayer.classList.add("player")
+  } else if (playersNum[1]) {
+    let newPlayer = document.createElement("div")
+    newPlayer.classList.add("player")
+  }
+}
+
+let num = 1
+for (let i = 0; i < 10; i++) {
+  let rows = document.createElement("div")
+  rows.classList.add("row")
+
+  for (let j = 0; j < 10; j++) {
+    let cells = document.createElement("div")
+    cells.classList.add("square")
+    cells.innerText = num
+    rows.appendChild(cells)
+    num++
+  }
+  game.appendChild(rows)
+}
+
+const squares = document.querySelectorAll(".square")
+console.log(squares)
+
+// num = 1
+// for (let i = 100; i >= 1; i--) {
+//   let cells = document.createElement("div")
+//   cells.classList.add("square")
+//   game.appendChild(cells)
+//   cells.innerText = num
+//   num++
+// }
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const getRandomNumber = () => {
@@ -33,20 +72,7 @@ const getRandomNumber = () => {
     squares[steps - 1].appendChild(player)
     winner.innerText = "Winner!"
   }
-
-  // } else if (steps === 10) {
-  //   winner.innerText = "Winner!"
-  // } else {
-
-  //   let finalSteps = 0
-  //   finalSteps = randomNum
-  //   console.log(finalSteps)
-  //   console.log(steps)
-  // if (currentSquare + finalSteps === 10) {
-  //   squares[9].appendChild(player)
-  //   winner.innerText = "Winner!"
-  // }
-  // }
 }
 
 dice.addEventListener("click", getRandomNumber)
+playerNumber.addEventListener("Click", playerNumber)
