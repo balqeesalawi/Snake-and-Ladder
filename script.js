5b10fedb6631c4bc3bfb15281779b6c250033a05
Balqeesalawi <balqeesalawi195@gmail.com>
2025-09-17 09:32:11 +0300

    created popUps and Added gifs for both snake and ladder

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let player = document.querySelectorAll(".player")
 const dice = document.querySelector("#dice")
 const winner = document.querySelector("#header")
+const update = document.querySelector("#update")
 const playersNum = document.querySelectorAll(".players")
 const playAgain = document.querySelector("#reset")
+const snakePopUp = document.querySelector("#snakePopUp")
+const ladderPopUp = document.querySelector("#ladderPopUp")
+const confirmation = document.querySelector(".confirm")
 
 let game = document.querySelector("#game")
 let gameOver = false
@@ -28,18 +32,16 @@ for (let i = 0; i < 10; i++) {
 }
 const squares = document.querySelectorAll(".square")
 
-squares[26].classList.add("snake")
-squares[50].classList.add("snake")
-squares[97].classList.add("snake")
-squares[91].classList.add("snake")
-let snakes = document.querySelectorAll(".snake")
-
 let snakeImg = document.createElement("img")
 snakeImg.setAttribute("src", "./snake.gif")
 snakeImg.style.width = "50px"
 snakeImg.style.height = "40px"
 
-squares[26].appendChild(snakeImg)
+squares[26].classList.add("snake")
+squares[50].classList.add("snake")
+squares[97].classList.add("snake")
+squares[91].classList.add("snake")
+let snakes = document.querySelectorAll(".snake")
 
 squares[7].classList.add("ladder")
 squares[58].classList.add("ladder")
@@ -47,66 +49,88 @@ squares[67].classList.add("ladder")
 squares[36].classList.add("ladder")
 let ladders = document.querySelectorAll(".ladder")
 
+const displaySnakePopUps = () => {
+  snakePopUp.style.opacity = 1
+  snakePopUp.style.animation = "bounceIn"
+  snakePopUp.style.animationDuration = "2s"
+  document.body.style.boxShadow = "inset 200px 200px 600px black"
+}
+
+const displayLadderPopUps = () => {
+  ladderPopUp.style.opacity = 1
+  ladderPopUp.style.animation = "bounceIn"
+  ladderPopUp.style.animationDuration = "2s"
+  document.body.style.boxShadow = "inset 200px 200px 600px black"
+}
+
+const checkForSnakeAndLadder = () => {
+  if (squares[steps - 1].innerText === "8") {
+    displayLadderPopUps()
+    squares[steps + 20].appendChild(player[playerTurn])
+
+    playerSteps[playerTurn] = parseInt(squares[steps + 20].innerText)
+  } else if (squares[steps - 1].innerText === "37") {
+    displayLadderPopUps()
+    squares[steps + 7].appendChild(player[playerTurn])
+
+    playerSteps[playerTurn] = parseInt(squares[steps + 7].innerText)
+  } else if (squares[steps - 1].innerText === "59") {
+    displayLadderPopUps()
+    squares[steps + 21].appendChild(player[playerTurn])
+
+    playerSteps[playerTurn] = parseInt(squares[steps + 21].innerText)
+  } else if (squares[steps - 1].innerText === "68") {
+    displayLadderPopUps()
+    squares[steps + 25].appendChild(player[playerTurn])
+
+    playerSteps[playerTurn] = parseInt(squares[steps + 25].innerText)
+  } else if (squares[steps - 1].innerText === "27") {
+    displaySnakePopUps()
+    squares[steps - 23].appendChild(player[playerTurn])
+
+    playerSteps[playerTurn] = parseInt(squares[steps - 23].innerText)
+  } else if (squares[steps - 1].innerText === "51") {
+    displaySnakePopUps()
+    squares[steps - 3].appendChild(player[playerTurn])
+
+    playerSteps[playerTurn] = parseInt(squares[steps - 3].innerText)
+  } else if (squares[steps - 1].innerText === "92") {
+    displaySnakePopUps()
+    squares[steps - 22].appendChild(player[playerTurn])
+
+    playerSteps[playerTurn] = parseInt(squares[steps - 22].innerText)
+  } else if (squares[steps - 1].innerText === "98") {
+    displaySnakePopUps()
+    squares[steps - 64].appendChild(player[playerTurn])
+
+    playerSteps[playerTurn] = parseInt(squares[steps - 64].innerText)
+  }
+
+  // if (snakes[i].appendChild(player[playerTurn])) {
+  //   displaySnakePopUps()
+  // } else if (ladders[i].appendChild(player[playerTurn])) {
+  //   displayLadderPopUps()
+  // }
+}
+
 const playGame = () => {
-  randomNum = Math.ceil(Math.random() * 6)
+  randomNum = 8
+
   randomNum = dice.innerText = randomNum
   steps += randomNum
+  update.innerText = `player ${playerTurn + 1} moved ${randomNum} steps`
 
   console.log("playerTurn", playerTurn)
   if (playerTurn === 0) {
     playerSteps[0] = steps
-  } else if (playerTurn === 1) {
-    playerSteps[1] = steps
-  } else if (playerTurn === 2) {
-    playerSteps[2] = steps
   } else {
-    playerSteps[3] = steps
+    playerSteps[1] = steps
   }
   console.log(playerSteps)
 
   if (steps < 100) {
     squares[steps - 1].appendChild(player[playerTurn])
-    // let poo = parseInt(squares[steps - 1].innerText)
-
-    // for (let i = steps; i < poo; i++) {
-    //   squares[i].append[playerTurn]
-    //   squares[steps - 1].appendChild(player[playerTurn])
-    // }
-    // console.log(poo)
-
-    if (squares[steps - 1].innerText === "8") {
-      squares[steps + 20].appendChild(player[playerTurn])
-
-      playerSteps[playerTurn] = parseInt(squares[steps + 20].innerText)
-    } else if (squares[steps - 1].innerText === "37") {
-      squares[steps + 7].appendChild(player[playerTurn])
-
-      playerSteps[playerTurn] = parseInt(squares[steps + 7].innerText)
-    } else if (squares[steps - 1].innerText === "59") {
-      squares[steps + 21].appendChild(player[playerTurn])
-
-      playerSteps[playerTurn] = parseInt(squares[steps + 21].innerText)
-    } else if (squares[steps - 1].innerText === "68") {
-      squares[steps + 25].appendChild(player[playerTurn])
-
-      playerSteps[playerTurn] = parseInt(squares[steps + 25].innerText)
-    } else if (squares[steps - 1].innerText === "27") {
-      squares[steps - 23].appendChild(player[playerTurn])
-
-      playerSteps[playerTurn] = parseInt(squares[steps - 23].innerText)
-    } else if (squares[steps - 1].innerText === "51") {
-      squares[steps - 3].appendChild(player[playerTurn])
-
-      playerSteps[playerTurn] = parseInt(squares[steps - 3].innerText)
-    } else if (squares[steps - 1].innerText === "92") {
-      squares[steps - 22].appendChild(player[playerTurn])
-
-      playerSteps[playerTurn] = parseInt(squares[steps - 22].innerText)
-    } else if (squares[steps - 1].innerText === "98") {
-      squares[steps - 64].appendChild(player[playerTurn])
-
-      playerSteps[playerTurn] = parseInt(squares[steps - 64].innerText)
-    }
+    checkForSnakeAndLadder()
   } else if (steps > 100) {
     playerSteps[playerTurn] = steps - randomNum
   } else {
@@ -116,7 +140,6 @@ const playGame = () => {
     steps = steps
   }
 
-  winner.innerText = `${playerTurn + 1} Turn!`
   if (!gameOver) {
     playerTurn++
     console.log(player.length)
